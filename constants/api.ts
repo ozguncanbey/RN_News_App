190edc7d3cb894b4d6c80732f40fdcc549b78bb3
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2025-04-24 15:43:07 +0300

    hide apikey

--- constants/api.ts
+++ constants/api.ts
@@ -1,5 +1,7 @@
 // constants/api.ts
 
-export const NEWS_API_KEY = 'fe5dd6ca68374ea4aff8c73550a00aaf'; // <<< KENDİ ANAHTARINIZLA DEĞİŞTİRDİĞİNİZDEN EMİN OLUN
+import { API_KEY } from '../constants/secrets'; // secrets.ts dosyasından API anahtarını import et
+
+export const NEWS_API_KEY = API_KEY; // API anahtarını export et
 
 export const NEWS_API_BASE_URL = 'https://newsapi.org/v2';
